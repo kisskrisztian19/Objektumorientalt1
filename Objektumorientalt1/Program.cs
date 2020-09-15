49705e58f261c2b1dc6cc83ae31b4d334f246a5d
krisztián kiss <kisskrisztiannnnn@hotmail.com>
2020-09-15 09:06:36 +0200

    Add project files.

---
+++ Objektumorientalt1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objektumorientalt1
+{
+    class Zember
+    {
+        public string nev;
+        public int eletkor;
+
+        public void bemutatkozas()
+        {
+            Console.WriteLine($"{nev} vagyok {eletkor} éves.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zember Pisti = new Zember();
+            Zember Eva = new Zember();
+
+            Pisti.nev = "Lakatos János";
+            Pisti.eletkor = 42;
+
+            Eva.nev = "Csutka Éva";
+            Eva.eletkor = 22;
+
+            Pisti.bemutatkozas();
+
+            Eva.bemutatkozas();
+
+            Console.ReadKey();
+        }
+    }
+}
